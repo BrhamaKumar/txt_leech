d95d5312d1fab19958c7b4c439c6a2854a8d8be1
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-03-17 13:18:02 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "20810825"
+api_hash = "707e67f53b4593a3e9b6b424311f84d0"
+bot_token = "6693648608:AAGAp9mwXLNdsD5MXsEMPtfHeNui3BixesE"
